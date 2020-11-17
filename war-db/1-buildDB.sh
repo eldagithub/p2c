d3c94ad753c8fffe52e97c047253308ba658ed4e
elmos@debian10 <elmos@debian10>
2020-11-17 13:11:28 +0100

    update DB checking

--- war-db/1-buildDB.sh
+++ war-db/1-buildDB.sh
@@ -2,6 +2,36 @@
 mkdir -p  target/datadir
 cp src/db.sql target/datadir/
 
-docker run --name=mysql1 -e MYSQL_ROOT_PASSWORD=pass -p 3306:3306    --mount type=bind,src=$(pwd)/src/my.cnf,dst=/etc/my.cnf    --mount type=bind,src=$(pwd)/target/datadir,dst=/var/lib/mysql    -d mysql:5.6
+#docker run --name=mysql1 -e MYSQL_ROOT_PASSWORD=pass -p 3306:3306    --mount type=bind,src=$(pwd)/src/my.cnf,dst=/etc/my.cnf    --mount type=bind,src=$(pwd)/target/datadir,dst=/var/lib/mysql    -d mysql:5.6
+
+echo "Checking ..... "
+
+cnt=0
+while true
+do
+
+
+if docker logs mysql1 2>/dev/stdout | grep "mysqld: ready for connections" 1>/dev/null 
+then 
+	echo "DB correctly started"  
+        exit 0
+fi
+
+((cnt++))
+
+if [[ "$cnt" == '10' ]]; then
+    
+  echo ""
+  echo ""
+  echo "Logs still not getting OK ..."
+  echo "Please check"
+  exit 10
+fi
+
+echo -n ".."
+sleep 2
+done
+
+
 
 
