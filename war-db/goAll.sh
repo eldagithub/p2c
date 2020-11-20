a96621901e73b54816a4e23cea74c5131e21af0d
el-mostafa.dahni@atleadit.com <el-mostafa.dahni@atleadit.com>
2020-11-20 14:31:39 +0100

    modif goAll.sh

--- war-db/goAll.sh
+++ war-db/goAll.sh
@@ -1,19 +1,19 @@
 #./0-docker-install.sh
-clear ; echo ./1-buildDB.sh
+
+sleep 5 ; clear  ; echo ./1-buildDB.sh
 ./1-buildDB.sh
 
-clear ; echo ./2-db-init.sh
+sleep 5 ; clear  ; echo ./2-db-init.sh
 ./2-db-init.sh
 
-clear ; echo ./3-tomcat-build.sh
+sleep 5 ; clear  ; echo ./3-tomcat-build.sh
 ./3-tomcat-build.sh
 
-clear ; echo ./4-tomcat-run.sh
+sleep 5 ; clear  ; echo ./4-tomcat-run.sh
 ./4-tomcat-run.sh
 
-sleep 10
-clear ; echo ./5-tomcat-test.sh
+sleep 15 ; clear  ; echo ./5-tomcat-test.sh
 ./5-tomcat-test.sh
 
-#clear ; echo ./6-tomcat-stop.sh
+#sleep 5 ; clear  ; echo ./6-tomcat-stop.sh
 #./6-tomcat-stop.sh
