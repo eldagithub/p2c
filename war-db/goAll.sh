81c1611dcb86a5a0ad6cd10f53446a34afe271ed
el-mostafa.dahni@atleadit.com <el-mostafa.dahni@atleadit.com>
2020-11-28 14:54:04 +0100

    war-db at Sam 28 nov 2020 14:54:04 CET

--- war-db/goAll.sh
+++ war-db/goAll.sh
@@ -7,20 +7,20 @@ echo "-----------------------------------------"
 groups
 echo "-----------------------------------------"
 
-sleep 5 ; clear  ; echo ./1-buildDB.sh
+sleep 5  ; echo ./1-buildDB.sh
 ./1-buildDB.sh
 
-sleep 5 ; clear  ; echo ./2-db-init.sh
+sleep 5  ; echo ./2-db-init.sh
 ./2-db-init.sh
 
-sleep 5 ; clear  ; echo ./3-tomcat-build.sh
+sleep 5  ; echo ./3-tomcat-build.sh
 ./3-tomcat-build.sh
 
-sleep 5 ; clear  ; echo ./4-tomcat-run.sh
+sleep 5  ; echo ./4-tomcat-run.sh
 ./4-tomcat-run.sh
 
 sleep 15 ; clear  ; echo ./5-tomcat-test.sh
 ./5-tomcat-test.sh
 
-#sleep 5 ; clear  ; echo ./6-tomcat-stop.sh
+#sleep 5  ; echo ./6-tomcat-stop.sh
 #./6-tomcat-stop.sh
