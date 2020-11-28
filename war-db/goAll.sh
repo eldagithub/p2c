67c295efe14ad7e545bc2a86ecb97b27421caef3
el-mostafa.dahni@atleadit.com <el-mostafa.dahni@atleadit.com>
2020-11-28 15:09:37 +0100

    war-db at Sam 28 nov 2020 15:09:37 CET

--- war-db/goAll.sh
+++ war-db/goAll.sh
@@ -1,4 +1,5 @@
 #./0-docker-install.sh
+#ret=$?;if [ ! $ret = 0 ]; then exit $ret; fi
 
 
 echo "-----------------------------------------"
@@ -9,18 +10,30 @@ echo "-----------------------------------------"
 
 sleep 5  ; echo ./1-buildDB.sh
 ./1-buildDB.sh
+ret=$?;if [ ! $ret = 0 ]; then exit $ret; fi
+
 
 sleep 5  ; echo ./2-db-init.sh
 ./2-db-init.sh
+ret=$?;if [ ! $ret = 0 ]; then exit $ret; fi
+
 
 sleep 5  ; echo ./3-tomcat-build.sh
 ./3-tomcat-build.sh
+ret=$?;if [ ! $ret = 0 ]; then exit $ret; fi
+
 
 sleep 5  ; echo ./4-tomcat-run.sh
 ./4-tomcat-run.sh
+ret=$?;if [ ! $ret = 0 ]; then exit $ret; fi
+
 
 sleep 15 ; clear  ; echo ./5-tomcat-test.sh
 ./5-tomcat-test.sh
+ret=$?;if [ ! $ret = 0 ]; then exit $ret; fi
 
 #sleep 5  ; echo ./6-tomcat-stop.sh
 #./6-tomcat-stop.sh
+#ret=$?;if [ ! $ret = 0 ]; then exit $ret; fi
+
+
